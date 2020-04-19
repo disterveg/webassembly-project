f6dbe941e3e43502df9fed6d12f3d3cedf063f1a
radrigo <cherkashin.da@gazprom-neft.ru>
2020-04-19 22:52:26 +0300

    Calling JavaScript functions from C-WASM

--- lib/imports.c
+++ lib/imports.c
@@ -5,5 +5,12 @@ int main() {
     printf("WASM ready\n");
 
     emscripten_run_script("console.log('hello from C')"); //Calling JavaScript functions from C
+
+    emscripten_async_run_script("console.log('hello from C')", 2000); //async
+
+    int jsVal = emscripten_run_script_int("getNum()");
+
+    printf("val from getNum(): %d\n",jsVal);
+
     return 1;
 }
