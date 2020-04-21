ec224bdffec060ca52611bf9a9b201d597e82458
radrigo <cherkashin.da@gazprom-neft.ru>
2020-04-21 17:12:37 +0300

    Preamble Conversion Functions

---
+++ lib/string.c
@@ -0,0 +1,14 @@
+#include <emscripten.h>
+#include <stdio.h>
+
+char * str = "my string variable";
+
+char * getStr() {
+    return str;
+}
+
+int main() {
+    printf("WASM ready\n");
+
+    return 1;
+}
